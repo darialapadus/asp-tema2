4b48aac64071165369c741f00b7bf2d4ccb4a3e5
darialapadus <127118850+darialapadus@users.noreply.github.com>
2023-10-12 19:51:11 +0300

    endpoint de delete by id (int), si un endpoint get care sa se numeasca "getAllOrdered" unde folositi .OrderBy(s => s.Age).ToList();

---
+++ Lab2_24-20231012T161544Z-001/Lab2_24/Lab2_24/Controllers/StudentsController.cs
@@ -0,0 +1,60 @@
+﻿using Lab2_24.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2_24.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        public static List<Student> students = new List<Student>
+        {
+            new Student { Id = 1, Name = "Ana", Age = 21},
+            new Student { Id = 12, Name = "Maria", Age = 19},
+            new Student { Id = 3, Name = "Vlad", Age = 22},
+            new Student { Id = 4, Name = "Florin", Age = 25},
+            new Student { Id = 5, Name = "Marian", Age = 20},
+        };
+
+        // endpoint 
+        // Get 
+        [HttpGet]
+        public List<Student> Get()
+        {
+            return students.OrderBy(o => o.Age).ToList();
+        }
+
+        // Create
+
+        [HttpPost]
+        public List<Student> Add(Student student)
+        {
+            students.Add(student);
+            return students;
+        }
+
+
+        [HttpDelete]
+        public List<Student> Delete(Student student)
+        {
+            var studentIndex = students.FindIndex( x => x.Id == student.Id);
+            students.RemoveAt(studentIndex);
+            return students;
+        }
+
+        [HttpDelete("{id:int}")]
+        public List<Student> DeleteById(int id)
+        {
+            var studentIndex = students.FindIndex(x => x.Id == id);
+            students.RemoveAt(studentIndex);
+            return students;
+        }
+
+        [HttpGet("ordered")]
+        public List<Student> GetAllOrdered()
+        {
+            return students.OrderBy(o => o.Id).ToList();
+        }
+    }
+}
